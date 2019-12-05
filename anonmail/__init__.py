fe9236815dabb2f584373f76b7f4138414effdd1
TheSpeedX <ggspeedx29@gmail.com>
2019-12-05 10:31:29 +0530

    Update to v1.2

--- anonmail/__init__.py
+++ anonmail/__init__.py
@@ -1,11 +1,10 @@
 
 __title__ = 'anonmail'
-__version__ = '1.1beta'
+__version__ = '1.2beta'
 __author__ = 'SpeedX'
 __license__ = 'GNU GENERAL PUBLIC LICENSE'
 __copyright__ = 'Copyright 2019 SpeedX'
 
-#from .anonmail import get_name,get_available_domain,get_username,get_email,fetch_emails,fetch_content
 from .anonmail import *
 
 
