4f37cfce9fd74ba9479861f163ca96947cfd178a
TheSpeedX <ggspeedx29@gmail.com>
2019-12-13 21:09:38 +0530

    Update to v1.3 Name Change and bug fixes

---
+++ fraudster/__init__.py
@@ -0,0 +1,10 @@
+
+__title__ = 'fraudster'
+__version__ = '1.3beta'
+__author__ = 'SpeedX'
+__license__ = 'GNU GENERAL PUBLIC LICENSE'
+__copyright__ = 'Copyright 2019 SpeedX'
+
+from .fraudster import *
+
+
